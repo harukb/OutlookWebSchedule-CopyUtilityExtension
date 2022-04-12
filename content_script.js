adcbed01c1e482f89d2d13049973143ec3a593c0
harukb@gmail.com <harukb@gmail.com>
2022-04-13 00:09:29 +0900

    images add/improve dom selection.

--- content_script.js
+++ content_script.js
@@ -1,21 +1,27 @@
 // content script例
-console.log("## MY EXTENSION IS ACTIVE ##");
+console.log("## OUTLOOKWEB_SCHEDULE_UTILITY_EXTENSION IS LOADED. ##");
 $(document).click(function(e){
-    e.preventDefault;
-    var id = e.target.id;
+    
     //1.日付(YYYY-MM-DD)のdom要素id
     var date_elem_class_str = "div.lYGICBIAS4cJG5gzkQZy";
-    //2.タイトル位置のdom要素id
+    //2.タイトル(スケジュールタスク名)のdom要素id
     var title_elem_class_str = "div.q2c0Pz_NVE8LxIICCwnB";
-    //3.時間帯のdom要素id
+    //3.時間帯(ex.16:00 - 18:00)のdom要素id
     var time_elem_class_str = "div.vnvfDJzs7TEd2MGb7YZa";
     
-    if (typeof id !== "undefined") {
-		if(id.startsWith("SearchResultItem_")){
-			var dayOfWeekStrJP = [ "日", "月", "火", "水", "木", "金", "土" ] ;
-			dt_text = $("#"+id).find(date_elem_class_str).text().split("-");
-			var date = new Date( +dt_text[0], +dt_text[1]-1, +dt_text[2]);
-			console.log($("#"+id).find(title_elem_class_str).text() + " " +  $("#"+id).find(date_elem_class_str).text() + "(" + dayOfWeekStrJP[date.getDay()] + ")" + " " + $("#"+id).find(time_elem_class_str).first().text());			
-		}
-    } 
+    //クリック対象の検索結果パネルを見つける
+    var $dom1 = $(e.target).closest('div[id^="SearchResultItem_"]');
+    
+	if($(e.target).closest('div[id^="SearchResultItem_"]').length){
+		//日にちから曜日を作成
+		var dayOfWeekStrJP = [ "日", "月", "火", "水", "木", "金", "土" ] ;
+		dt_text = $dom1.find(date_elem_class_str).text().split("-");
+		var date = new Date( +dt_text[0], +dt_text[1]-1, +dt_text[2]);
+		//console.logに出力
+		console.log($dom1.find(title_elem_class_str).text() 
+			+ " " +  $dom1.find(date_elem_class_str).text() + "(" + dayOfWeekStrJP[date.getDay()] + ")" 
+			+ " " + $dom1.find(time_elem_class_str).first().text());			
+	} else {
+		console.log('対象が存在しません');
+	}
 });
